aae03d4ad18b87f76e621c5cfb36b4851ee08959
kalsaitgaurav@gmail.com <kalsaitgaurav@gmail.com>
2026-02-16 11:16:53 +0530

    Added multi-AZ public and private subnets

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "aws_vpc" "main" {
 
 data "aws_availability_zones" "available" {}
 
+# Public Subnets
 resource "aws_subnet" "public" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
@@ -20,5 +21,19 @@ resource "aws_subnet" "public" {
 
   tags = {
     Name = "public-subnet-${count.index + 1}"
+    Type = "Public"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+    Type = "Private"
   }
 }
